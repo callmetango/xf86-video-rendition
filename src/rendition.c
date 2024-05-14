49d50f1fdeb4dc81895fec81be9fae1dc78e5974
Enrico Weigelt, metux IT consult <info@metux.net>
2024-05-14 15:25:11 +0200

    drop compat with ancient xserver version
    We're relying on at least 1.18 now
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>
    Part-of: <https://gitlab.freedesktop.org/xorg/driver/xf86-video-rendition/-/merge_requests/4>

--- src/rendition.c
+++ src/rendition.c
@@ -654,11 +654,7 @@ renditionPreInit(ScrnInfoPtr pScreenInfo, int flags)
     vgaHWSetStdFuncs(pvgaHW);
     vgaHWGetIOBase(pvgaHW);             /* Get VGA I/O base */
 
-#if GET_ABI_MAJOR(ABI_VIDEODRV_VERSION) < 12
-    pRendition->board.vgaio_base = pvgaHW->PIOOffset;
-#else
     pRendition->board.vgaio_base = 0;
-#endif
     pRendition->board.io_base = pRendition->board.vgaio_base 
 #ifdef XSERVER_LIBPCIACCESS
 	+ pRendition->PciInfo->regions[1].base_addr;
